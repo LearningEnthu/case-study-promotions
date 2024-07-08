a985a0baf7fe424d5ee153ae351642625aff8ce5
Arpit Porwal <arpit.porwal@inmobi.com>
2024-07-08 07:57:07 +0530

    added project skeleton

---
+++ src/main/java/com/personal/case_study_promotions/CaseStudyPromotionsApplication.java
@@ -0,0 +1,13 @@
+package com.personal.case_study_promotions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyPromotionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseStudyPromotionsApplication.class, args);
+	}
+
+}
